e48b9178dcd2e664061bed53da2fe20e590ddd3a
Kstabile82 <87687726+Kstabile82@users.noreply.github.com>
2021-10-10 12:08:48 -0400

    Add files via upload

---
+++ Rabbits.js
@@ -0,0 +1,12 @@
+//choose region or state 
+//answer the rest of the drop downs
+//conditionals: if ears && color && region && the rest of it, identify rabbit
+//else if region && size && color
+//if return swampRabbit:
+//if return marshRabbit:
+//if return easternCottontail:
+//if return jackRabbit:
+//fetch request on the returned rabbit? insert frame and image into html? 
+//when returning rabbit, console log message "it sounds like you're looking at a ___ or a ___. Here are pictures and some further details to help you determine!"
+//when returning domestic, console log: "You might be looking at a domestic rabbit that's been abandoned and is at risk. Please try reaching out to an animal rescue local to your area to see if they can determine and get it to safety."
+//when maybe each, "It sounds like you're looking at a ___, but it could also be a domestic rabbit that needs help. Here are some images of ____ so that you can compare. If you think it's domestic, please try reaching out to an animal rescue local to your area to see if they can determine and get it to safety."
